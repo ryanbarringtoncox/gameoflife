5202fd7cfbffe05df2110d2f09eadc0aeb731777
core.editor <ryanbarrington@gmail.com>
2013-07-31 14:46:24 -0400

    prints world

--- gameoflife.js
+++ gameoflife.js
@@ -41,7 +41,7 @@ var World = function(x,y) {
     },
 
     getNeighborCount: function(x,y) {
-      if (lives[x+"_"+y]) {
+      if (this.hasLife(x,y)) {
         var living = 0;
         var candidates = getNeighborhood(x,y);
         candidates.forEach(function(c) {
@@ -72,8 +72,28 @@ var World = function(x,y) {
       };
     }, 
 
+    printWorld: function() {
+      for (var i = 0; i < y; i = i + 1) {
+        var currLine = ""; 
+        for (var j = 0; j < x; j = j + 1) {
+          if (this.hasLife(j,i)) {
+            currLine += "x";
+          } else {
+            currLine += " "; 
+          }
+        }
+        console.log(currLine);
+      }
+    },
+
     removeLife: function(x,y) {
       delete lives[x+"_"+y];
+    },
+
+    update: function() {
+      for (var key in lives) {
+        console.log(key);
+      }
     }
   }
 
@@ -90,13 +110,22 @@ world.insertLife(11,1);
 world.insertLife(1,21);
 world.insertLife(1,31);
 world.insertLife(1,91);
-world.printLives();
+//world.printLives();
 assert("testing hasLife()", true===world.hasLife(1,1));
 assert("testing hasLife()", false===world.hasLife(99,1));
 world.removeLife(1,1);
 assert("Testing removeLife()", false===world.hasLife(1,1));
 world.insertLife(1,1);
 assert("Testing getNeighborCount()", -1===world.getNeighborCount(91,1));
-//assert("Testing getNeighborCount()", 1===world.getNeighborCount(1,1));
-
+assert("Testing getNeighborCount()", 1===world.getNeighborCount(1,1));
 assert("Testing getNeighborCount()", 1===world.getNeighborCount(0,0));
+assert("Testing getNeighborCount()", 0===world.getNeighborCount(22,1));
+delete world;
+
+var w = new World(10,10);
+w.insertLife(4,4);
+w.insertLife(4,5);
+w.insertLife(4,6);
+w.printWorld();
+w.update();
+//w.printWorld();
