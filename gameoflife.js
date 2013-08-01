ef1b8f80b4dc82d440db49a7fea3d9eccd673319
core.editor <ryanbarrington@gmail.com>
2013-08-01 10:43:12 -0400

    adds error handling

--- gameoflife.js
+++ gameoflife.js
@@ -1,38 +1,13 @@
-var World = require("./world.js"), assert;
-
-assert = function(descrip, stmt) {
-  if (stmt) {
-    console.log("PASS: " + descrip);
-  } else {
-    console.log("FAIL: " + descrip);
-  }
-}
-
-var world = new World(100, 100);
-assert("Tests insertLife()", 1!==world.insertLife(0,0));
-assert("Tests insertLife()", -1===world.insertLife(100,0));
-world.insertLife(0,0);
-world.insertLife(1,1);
-world.insertLife(22,1);
-world.insertLife(11,1);
-world.insertLife(1,21);
-world.insertLife(1,31);
-world.insertLife(1,91);
-assert("testing hasLife()", true===world.hasLife(1,1));
-assert("testing hasLife()", false===world.hasLife(99,1));
-world.removeLife(1,1);
-assert("Testing removeLife()", false===world.hasLife(1,1));
-world.insertLife(1,1);
-assert("Testing getNeighborCount()", -1===world.getNeighborCount(91,1));
-assert("Testing getNeighborCount()", 1===world.getNeighborCount(1,1));
-assert("Testing getNeighborCount()", 1===world.getNeighborCount(0,0));
-assert("Testing getNeighborCount()", 0===world.getNeighborCount(22,1));
-delete world;
-
+var World = require("./world.js");
 var w = new World(10,10);
 w.insertLife(4,4);
 w.insertLife(4,5);
 w.insertLife(4,6);
+w.insertLife(9,9);
+w.insertLife(9,8);
+w.insertLife(8,9);
+w.insertLife(8,8);
+w.toString();
 w.printWorld();
 w.update();
 w.printWorld();
