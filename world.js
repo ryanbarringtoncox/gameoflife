4f1c54cda246f03302afa6b953877bca32fd7e26
core.editor <ryanbarrington@gmail.com>
2013-08-01 10:25:56 -0400

    re-organizes files and objects

--- world.js
+++ world.js
@@ -1,33 +1,26 @@
 function World(x,y) {
 
-  var getNeighborhood, getNeighborhoodString, width, height, lives, offsets;
+  var width, height, lives, offsets;
 
   width = x;
   height = y;
   lives = {}; 
   offsets = [[-1,-1], [-1,0],  [-1,1],  [0,-1],  [0,1],  [1,-1],  [1,0],  [1,1] ];
 
-  getNeighborhood = function(x,y) {
-    var neighborhood = [];
-    offsets.forEach(function(offset) {
-      var curr = (x+offset[0]) + "_" + (y+offset[1]);
-      neighborhood.push(curr);
-    });
-    return neighborhood;
-  }; 
-
-  getNeighborhoodString = function(life) {
-    var nums = life.split("_");
-    var nabeHood = getNeighborhood(parseInt(nums[0]), parseInt(nums[1]));
-    return nabeHood;
-  };
-
   return {
 
     toString: function() {
       console.log("This world is " + width + " by " + height);
+      console.log(Object.keys(lives).length + " lives are:");
+      for (var key in lives) {
+        console.log(key);
+      }; 
+    }, 
+
+    stringIt: function(x,y) {
+      return x+"_"+y; 
     },
-   
+
     hasLife: function(x,y) {
       if (lives[x+"_"+y]) {
         return true;
@@ -36,9 +29,24 @@ function World(x,y) {
       }
     },
 
-    getNeighborCount: function(x,y) {
+    getNeighborhood: function(x,y) {
+      var neighborhood = [];
+      offsets.forEach(function(offset) {
+        var curr = (x+offset[0]) + "_" + (y+offset[1]);
+        neighborhood.push(curr);
+      });
+      return neighborhood;
+    },
+
+    getNeighborhoodString: function(life) {
+      var nums = life.split("_");
+      var nabeHood = this.getNeighborhood(parseInt(nums[0]), parseInt(nums[1]));
+      return nabeHood;
+    },
+
+    getLiveNabeCount: function(x,y) {
         var living = 0;
-        var candidates = getNeighborhood(x,y);
+        var candidates = this.getNeighborhood(x,y);
         candidates.forEach(function(c) {
           if (lives[c]) {
             living = living + 1; 
@@ -47,9 +55,9 @@ function World(x,y) {
         return living;  
     },
 
-    getNabeCountString: function(nabe) {
+    getLiveNabeCountString: function(nabe) {
         var nums = nabe.split("_");
-        var count = this.getNeighborCount(parseInt(nums[0]), parseInt(nums[1]));
+        var count = this.getLiveNabeCount(parseInt(nums[0]), parseInt(nums[1]));
         return count;
     },
 
@@ -62,13 +70,6 @@ function World(x,y) {
         return lives;
       }
     },
-
-    printLives: function() {
-      console.log(Object.keys(lives).length + " lives are:");
-      for (var key in lives) {
-        console.log(key);
-      };
-    }, 
 
     printWorld: function() {
       for (var i = 0; i < y; i = i + 1) {
@@ -93,19 +94,19 @@ function World(x,y) {
       var self = this;
       for (var key in lives) {
         //will this life survive?
-        var count = this.getNabeCountString(key);
+        var count = this.getLiveNabeCountString(key);
         //console.log(key + " has " + count + " living neighbors");
         if (count === 2 || count ===3) {
           //console.log(key + " will survive");
           futureWorld[key] = true;
         }
         //will birth occur?
-        var nabes = getNeighborhoodString(key);
+        var nabes = this.getNeighborhoodString(key);
         //console.log("nabes are " + nabes);
         nabes.forEach(function(nabe) {
           if (!futureWorld[nabe]) {
             //console.log(nabe + " not in futureWorld yet");
-            var count = self.getNabeCountString(nabe);
+            var count = self.getLiveNabeCountString(nabe);
             if (count === 3) {
               //console.log(nabe + " is born");
               futureWorld[nabe] = true;
