8253a496bfde500dd746f06a19762b2734d18020
core.editor <ryanbarrington@gmail.com>
2013-08-14 12:56:04 -0400

    modularizes drumsprite

--- app/main.js
+++ app/main.js
@@ -1,20 +1,9 @@
 var World = require('world');
+var DrumSprite = require('drumsprite');
+var sound = new DrumSprite();
 
 var w = new World(10,10);
 w.toString();
-
-var sound = new Howl({
-urls: ['drums.mp3'],
-  sprite: {
-    1: [0,500],
-    2: [1000,500],
-    3: [2000,500],
-    4: [3000,500],
-    5: [4000,500],
-    6: [5000,500],
-    7: [6000,500],
-  }
-});
 
 
 var playIt = function(key, sound) {
