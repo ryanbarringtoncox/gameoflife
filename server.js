9567a140f53f668a98e4df414542276ef70a859e
Ryan Barrington Cox <ryanbarrington@gmail.com>
2013-09-13 09:58:56 -0400

    mods heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //for heroku deployment
 var statik = require('statik');
-var server = statik.createServer();
-server.listen(process.env.PORT || 1337);
+//var server = statik.createServer();
+//server.listen(process.env.PORT || 1337);
+statik(3000);
