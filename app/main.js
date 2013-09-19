93d7f95350401c5eec3cfffaa4d09dca586b2d6d
ryanbarringtoncox <ryanbarrington@gmail.com>
2013-09-18 22:48:17 -0400

    lints code, reduces canvas size

--- app/main.js
+++ app/main.js
@@ -4,7 +4,9 @@
     Canvas = require('canvas'),
     PianoSprite = require('./audio/pianosprite'),
     sound, w, canvas, startBtn, interval, slider;
-
+  
+  //default time tick in ms
+  interval = 300;
 
   canvas = new Canvas(document, 'main');
   canvas.init();
@@ -17,18 +19,15 @@
     interval = this.value;
   });
   
-  //how often game ticks in ms
-  interval = 300;
-
   w = new World(canvas.width-1,canvas.height-1,10);
 
   var gameLoop = function () {
-  capturedCells = canvas.captureCells();
-    w.insertLives(capturedCells);
-    w.update();
-    canvas.clearCapturedCells();
-    canvas.render(w);
-    setTimeout(gameLoop,interval);
+    capturedCells = canvas.captureCells();
+      w.insertLives(capturedCells);
+      w.update();
+      canvas.clearCapturedCells();
+      canvas.render(w);
+      setTimeout(gameLoop,interval);
   }
 
   startBtn.click(function() {
