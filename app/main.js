9e15daa02d18a6fbde4eb9bd29e5d5898b909f86
ryanbarringtoncox <ryanbarrington@gmail.com>
2013-09-21 13:30:13 -0400

    removes unhelpful code

--- app/main.js
+++ app/main.js
@@ -5,37 +5,6 @@
     PianoSprite = require('./audio/pianosprite'),
     sound, w, canvas, startBtn, interval, slider;
   
-  var canvasElement = document.getElementById('main');
-
-  // do nothing in the event handler except canceling the event
-  canvasElement.ondragstart = function(e) {
-      if (e && e.preventDefault) { e.preventDefault(); }
-      if (e && e.stopPropagation) { e.stopPropagation(); }
-      return false;
-  }
-
-  // do nothing in the event handler except canceling the event
-  canvasElement.onselectstart = function(e) {
-      if (e && e.preventDefault) { e.preventDefault(); }
-      if (e && e.stopPropagation) { e.stopPropagation(); }
-      return false;
-  }
-
-  /*
-  //cancel mobile window movement
-  document.body.ontouchstart = function(e) {
-      if (e && e.preventDefault) { e.preventDefault(); }
-      if (e && e.stopPropagation) { e.stopPropagation(); }
-      return false;
-  }
-
-  document.body.ontouchmove = function(e) {
-      if (e && e.preventDefault) { e.preventDefault(); }
-      if (e && e.stopPropagation) { e.stopPropagation(); }
-      return false;
-  }
-  */
-
   //default time tick in ms
   interval = 300;
 
